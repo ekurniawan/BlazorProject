2395d85bff114fd65251ca9e092e865d5781d86c
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-11 14:17:26 +0700

    menambahkan component EmployeeList

---
+++ BlazorProject/Client/Pages/TodoBase.cs
@@ -0,0 +1,24 @@
+﻿using BlazorProject.Client.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Client.Pages
+{
+    public class TodoBase : ComponentBase
+    {
+        public List<TodoItem> todos { get; set; } = new();
+        protected string newTodo;
+
+        protected void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+    }
+}
