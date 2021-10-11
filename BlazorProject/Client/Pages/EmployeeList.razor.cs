3b74978b1e789b69c5b8bfa0d2a6451361e7fb57
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-11 14:30:15 +0700

    menambahkan data employee

--- BlazorProject/Client/Pages/EmployeeList.razor.cs
+++ BlazorProject/Client/Pages/EmployeeList.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlazorProject.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,39 @@ namespace BlazorProject.Client.Pages
 {
     public partial class EmployeeList
     {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+
+        private void LoadEmployee()
+        {
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Erick",
+                LastName = "Kurniawan",
+                Email = "erick@gmail.com",
+                DateOfBirth = new DateTime(1990, 1, 1),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                PhotoUrl = "images/pic1.jpg"
+            };
+            Employee e2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Peter",
+                LastName = "Parker",
+                Email = "peter@gmail.com",
+                DateOfBirth = new DateTime(1990, 2, 2),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 1, DepartmentName = "HRD" },
+                PhotoUrl = "images/pic2.jpg"
+            };
+
+            Employees = new List<Employee> { e1, e2 };
+        }
     }
 }
