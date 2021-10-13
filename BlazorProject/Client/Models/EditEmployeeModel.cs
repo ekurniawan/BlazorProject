cdac0fe2f405bd8cf29f76d6ae8ef2fd7ed66392
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-13 15:41:23 +0700

    menambahkan viewmodel EditEmployeeModel

--- BlazorProject/Client/Models/EditEmployeeModel.cs
+++ BlazorProject/Client/Models/EditEmployeeModel.cs
@@ -1,29 +1,32 @@
-﻿using BlazorProject.Shared.CustomValidators;
+﻿using BlazorProject.Shared;
+using BlazorProject.Shared.CustomValidators;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorProject.Shared
+namespace BlazorProject.Client.Models
 {
-    public class Employee
+    public class EditEmployeeModel
     {
         public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "First Name harus diisi data")]
+        [StringLength(100, MinimumLength = 2)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name harus diisi data")]
         public string LastName { get; set; }
+
+        [Required]
+        [EmailDomainValidator(AllowedDomain = "gmail.com")]
         public string Email { get; set; }
+        public string ConfirmEmail { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
         public string PhotoUrl { get; set; }
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
-    }
-
-    public enum Gender
-    {
-        Male,
-        Female
     }
 }
---
+++ BlazorProject/Shared/CustomValidators/EmailDomainValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Shared.CustomValidators
+{
+    public class EmailDomainValidator : ValidationAttribute
+    {
+        public string AllowedDomain { get; set; }
+        protected override ValidationResult IsValid(object value, 
+            ValidationContext validationContext)
+        {
+            string[] arrEmail = value.ToString().Split('@');
+            if(arrEmail.Length>1 && arrEmail[1].ToUpper() == AllowedDomain.ToUpper())
+            {
+                return null;
+            }
+            return new ValidationResult($"Domain pada email harus {AllowedDomain}",
+                new[] { validationContext.MemberName });
+        }
+    }
+}
