e293a13f038550ce0016ab433fa58f709f235ba4
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-13 13:20:38 +0700

    menambahkan code component editemployee

---
+++ BlazorProject/Client/Pages/EditEmployee.razor.cs
@@ -0,0 +1,26 @@
+﻿using BlazorProject.Client.Services;
+using BlazorProject.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Client.Pages
+{
+    public partial class EditEmployee
+    {
+        public Employee Employee { get; set; } = new Employee();
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetById(Convert.ToInt32(Id));
+        }
+    }
+}
