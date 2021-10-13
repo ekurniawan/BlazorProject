a5f42e3d8757bfe868e6ed579bea3c6a5a131cb4
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-13 13:38:45 +0700

    menambahkan department controller backend

---
+++ BlazorProject.Server/Controllers/DepartmentsController.cs
@@ -0,0 +1,75 @@
+﻿using BlazorProject.Server.Models;
+using BlazorProject.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorProject.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private IDepartmentRepository _departmentRepository;
+        public DepartmentsController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        // GET: api/<DepartmentsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> Get()
+        {
+            try
+            {
+                var result = await _departmentRepository.GetAll();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "" +
+                    "Gagal mengembalikan nilai department");
+            }
+        }
+
+        // GET api/<DepartmentsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> Get(int id)
+        {
+            try
+            {
+                var result = await _departmentRepository.GetById(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                                    $"Kesalahan: {ex.Message}");
+            }
+        }
+
+        // POST api/<DepartmentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DepartmentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DepartmentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
