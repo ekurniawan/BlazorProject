47453d509b6dda10e1b5c8fb2cbbece291add971
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-14 11:53:36 +0700

    menambahkan confirmation component

---
+++ Act.Components/Confirm.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act.Components
+{
+    public partial class Confirm : ComponentBase
+    {
+        public bool ShowConfirmation { get; set; }
+        [Parameter]
+        public string ConfirmationTitle { get; set; } = "Konfirmasi Hapus Data";
+        [Parameter]
+        public string ConfirmationMessage { get; set; } = "Apakah anda yakin untuk delete data?";
+
+        public void Show()
+        {
+            ShowConfirmation = true;
+            StateHasChanged();
+        }
+
+        [Parameter]
+        public EventCallback<bool> ConfirmationChanged { get; set; }
+
+        protected async Task OnConfirmationChange(bool value)
+        {
+            ShowConfirmation = false;
+            await ConfirmationChanged.InvokeAsync(value);
+        }
+
+    }
+}
