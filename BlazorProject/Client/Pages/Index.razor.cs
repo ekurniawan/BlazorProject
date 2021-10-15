027ec37983105cb3ba5d97421e9d6f59a930842b
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-15 10:50:57 +0700

    menambahkan index

---
+++ BlazorProject/Client/Pages/Index.razor.cs
@@ -0,0 +1,26 @@
+﻿using BlazorProject.Client.Services;
+using BlazorProject.Shared;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Client.Pages
+{
+    [Authorize]
+    public partial class Index
+    {
+        private bool loading;
+        private IEnumerable<User> users;
+
+        public IUserService UserService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            loading = true;
+            users = await UserService.GetAll();
+            loading = false;
+        }
+    }
+}
